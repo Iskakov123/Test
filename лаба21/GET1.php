1fd16006ca2a23f33c91dc06f1124b8e79ffe14e
Iskakov123 <89969965+Iskakov123@users.noreply.github.com>
2021-12-06 18:51:06 +0300

    Update GET1.php

--- лаба21/GET1.php
+++ лаба21/GET1.php
@@ -1,49 +1,36 @@
+<?
+  $bg_color="#fff";
+  if(isset($_GET["iv_color"]))
+    $bg_color=$_GET["iv_color"];
+?>
 <!doctype html>
-
-<?
-if (isset($GET["iv_color"]))
-	print_r($GET["iv_color"]);
-?>
-
-<?
-$bg_color="#fff";
-if (isset($_GET["iv_color"]))
-	$bg_color=$_GET["iv_color"];
-	else
-	$bg_color="#fff";
-?>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
+    < metacharset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	</head>
-	
-    <form class="/Iskakov/LR21/" method="GET">
-  <div class="mb-3">
-    <label for="exampleColorInput" class="form-label">Color Picker</label>
-    <input name="iv_color" type="color" class="form-control form-control-color" id="exampleColorInput" value="#ffffff" title="Choose your color">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-  
-  <body style="background-color:<?=$bg_color?>">
-		<div class="container">
-		<h1><?
-		echo "Выбери любимый цвет"?></h1>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  </head>
+  < bodystyle="background-color: <?=$bg_color?>">
+    <h1>Выбери любимый цвет</h1>
+    <form action="/Iskakov/LR21/" method="GET">
+      <div class="mb-3">
+        <label for="exampleColorInput" class="form-label">Color picker</label>
+        <input name="iv_color" type="color" class="form-control form-control-color" value="#ffffff" title="Choose your color"></div>
+      <button type="submit" class="btn btn-primary">Submit</button>      
+    </form>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
     -->
   </body>
 </html>
